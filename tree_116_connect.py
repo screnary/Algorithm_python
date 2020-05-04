c1e3bbf228595b2682a3e84c4638ef3bee27a41a
screnary <screnary@qq.com>
2020-05-04 16:45:25 +0800

    2020.05.04, tree exercise

---
+++ tree_116_connect.py
@@ -0,0 +1,30 @@
+"""
+填充它的每个 next 指针，让这个指针指向其下一个右侧节点。
+如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+初始状态下，所有 next 指针都被设置为 NULL。
+"""
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution:
+    def connect(self, root):
+        if root is None: return None
+        queue = [root]
+        while queue:
+            n = len(queue)
+            for i in range(n):
+                node = queue.pop(0)
+                if i < n-1 and queue:  # the last node in this layer, do not connect
+                    node.next = queue[0]
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return root
